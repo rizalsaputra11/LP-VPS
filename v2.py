0d0ab3f2eb76cac0f2de71cfa6e6aeacb8b97930
MR_SDBD <mrsdbdop2@gmail.com>
2025-06-25 02:32:55 +0600

    Update v2.py

--- v2.py
+++ v2.py
@@ -17,13 +17,13 @@ from datetime import datetime, timedelta
 from typing import Optional, Literal
 
 TOKEN = ''
-RAM_LIMIT = '64g'
+RAM_LIMIT = '5g'
 SERVER_LIMIT = 1
 database_file = 'database.txt'
 PUBLIC_IP = '138.68.79.95'
 
 # Admin user IDs - add your admin user IDs here
-ADMIN_IDS = [1258646055860568094,1330372816926670979,951418376230670336,1295737579840340032]  # Replace with actual admin IDs
+ADMIN_IDS = [1339181629855502418,1368602087520473140]  # Replace with actual admin IDs
 
 intents = discord.Intents.default()
 intents.messages = False
